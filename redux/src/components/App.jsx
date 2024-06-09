2a74c71e3c2ae6aaed0c9fe8c12c1a77329689c9
Dev-SK01 <srik3713@gmail.com>
2024-06-09 12:33:25 +0530

    Fp Completed | redux pkg installed

---
+++ redux/src/components/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./css/App.css";
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl text-center font-bold bg-blue-600 ">
+        Hello world!
+      </h1>
+      <h2 className="text-3xl text-center font-extralight ">Hello world 2</h2>
+    </>
+  );
+}
+
+export default App;
---
+++ redux/src/fp/functionalProgramming.js
@@ -0,0 +1,53 @@
+/*
+  Main Goal is get a string from user and trim that string wap in html element. 
+  Implementing Functional Programming Concepts.
+*/
+const {compose , pipe } = require('lodash/fp')  
+function trimStr(str){
+    return str.trim();
+}
+
+function wrapInElement(elem){
+   return str => {
+    return `<${elem}>${str}</${elem}>`
+   }
+}
+
+function asArgument(fn , data){
+   console.log(fn(data));
+}
+
+function returnAsFunction(a){
+  return function (b){
+    return a + b ;
+  }
+}
+
+
+
+const useCompose = compose(wrapInElement('h1') , trimStr);
+const usePipe = pipe(trimStr , wrapInElement('span'));
+const currying = returnAsFunction;
+const person = {
+  name : 'Dev' , 
+  role  : {
+    dept : 'software Dev' ,
+    salary  : 40000
+  }
+}
+
+const immutableObject = {
+  ...person , 
+  name : 'Sid Dev' ,
+  role : {...person.role , dept : 'testing' , salary : 30000}
+} 
+
+console.log(person);
+console.log(immutableObject);
+console.log(currying(10)(20));
+asArgument(trimStr ,'  hello  ') ;
+console.log(useCompose(' hello world! '))
+console.log(usePipe(' hello world! '));
+
+
+
